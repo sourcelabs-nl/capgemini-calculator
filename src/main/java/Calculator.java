458c788283741e31e8e8e798378058b81229be16
Koehler, A (Arno) <arno.koehler@rabobank.nl>
2018-09-26 19:50:37 +0200

    initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int sum(int c, int d) {
+        return c + d;
+    }
+
+    public int subtract(int first, int second) {
+        return first + second;
+    }
+
+    public int maxValue(int[] values) {
+        int max = 0;
+
+        for (final int value : values) {
+            if (max < value) {
+                max = value;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void shouldMultiplyTwoNumbers() {
+        // call the multiply method and verify the result is correct
+    }
+
+    @Test
+    public void shouldDivideTwoNumbers() {
+        // call the divide method and verify the result is correct
+    }
+
+    @Test
+    public void shouldSumTwoNumbers() {
+        // call the sum method and verify the result is correct
+    }
+
+    @Test
+    public void shouldSubtractTwoNumbers() {
+        // call the subtract method - can you spot the bug?
+        assertEquals(3, calculator.subtract(6,3));
+    }
+
+}
